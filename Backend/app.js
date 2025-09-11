fbc41f6f6ebc3822948ba13b60689fc660112c09
Tanishq Sonwane <tanishqsonwane32@gmail.com>
2025-09-11 17:41:03 +0530

    new code dia

--- Backend/app.js
+++ Backend/app.js
@@ -1,35 +1,45 @@
 import dotenv from 'dotenv';
-dotenv.config(); 
-import {join, dirname} from 'path'
+dotenv.config();
+
 import express from 'express';
 import cors from 'cors';
-import cookieParser  from 'cookie-parser'
+import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 import userRoutes from './routes/user.routes.js';
 import projectRoutes from './routes/project.routes.js';
 import airRoutes from './routes/ai.routes.js';
+
 const app = express();
 
-app.use(cookieParser());
+// Create __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.use(express.static(join('public')));
+app.use(cookieParser());
+app.use(express.static(join(__dirname, 'public')));
 
 app.use(cors({
   credentials: true,
   origin: 'https://bixi-devin.onrender.com'
 }));
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/users', userRoutes);
+app.use('/projects', projectRoutes);
+app.use('/ai', airRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello bigmouse');
 });
-app.use('/projects', projectRoutes);
-app.use('/ai', airRoutes);
+
 app.get('*name', (req, res) => {
-  res.sendFile(join(dirname,"..","/public/index.html"));
+  res.sendFile(join(__dirname, 'public', 'index.html'));
 });
+
 export default app;
